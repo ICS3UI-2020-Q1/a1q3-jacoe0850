78925165691187e8846dbd9eb92da02437fc8bc8
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-14 15:25:10 +0000

    Finished computer parts cost

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,20 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    // Asking for quantities 
+    System.out.println("How many chromebook chargers?");
+     int chargers = input.nextInt();
+      System.out.println("How many motherboards?");
+      int motherboards = input.nextInt();
+      System.out.println("How many mice?");
+      int mice = input.nextInt();
+      // calculating basic subtotal
+double subtotal = chargers*34.99+motherboards*127.50+mice*18.00;
+// stating subtotal
+System.out.println("Subtotal= $" + subtotal);
+//stating and calculating taxes and total with taxes
+System.out.println("Subtotal= $" + subtotal*0.13);
+System.out.println("Subtotal= $" + subtotal*1.13);
   }
 }
